93adffe2a115e489c6eac4c2453b0be5fca51052
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2017-05-26 14:19:39 -0700

    added a script for combining together integrations.

---
+++ comb_img.py
@@ -0,0 +1,76 @@
+from astropy.io import fits
+import numpy as np
+import glob
+import pdb
+import os
+
+class combInts():
+    """ Object that can combine individual integration files made by pynrc into a single FITS """
+    def __init__(self,fileString,outImg='comb_img.fits'):
+        self.outImg = outImg
+        self.fileString = fileString
+        self.fileList = glob.glob(fileString)
+        if len(self.fileList) >= 1:
+            self.firstFile = self.fileList[0]
+        else:
+            print("No files found for "+self.fileString)
+            pdb.set_trace()
+        
+        HDUList = fits.open(self.firstFile)
+        self.firstHeader = HDUList['PRIMARY'].header
+        self.nints = self.firstHeader['NINTS']
+        self.ngroups = self.firstHeader['NGROUPS']
+        self.subsizeX = self.firstHeader['SUBSIZE1']
+        self.subsizeY = self.firstHeader['SUBSIZE2']
+        self.firstSciHeader = HDUList['SCI'].header
+        #self.nx = self.
+        HDUList.close()
+        
+    def checkDetector(self):
+        """ Checks that the detector keyword is the same in all images """
+        checkKeys = ['DETECTOR','NGROUPS','SUBSIZE1','SUBSIZE2']
+        for oneFile in self.fileList:
+            HDUList = fits.open(oneFile)
+            thisHeader = HDUList[0].header
+            for oneKey in checkKeys:
+                if thisHeader[oneKey] != self.firstHeader[oneKey]:
+                    print('Error: More than one '+oneKey+' in FITS list')
+                    print(oneFile,' : ',thisHeader[oneKey])
+                    print(self.firstFile, ' : ',self.firstHeader[oneKey])
+    
+    def comb(self):
+        """ Combines the files """
+        self.checkDetector()
+        allCube = np.zeros((self.nints,self.ngroups,self.subsizeY,self.subsizeX),dtype='uint16')
+        for intInd, oneFile in enumerate(self.fileList):
+            HDUList = fits.open(oneFile)
+            allCube[intInd,:,:,:] = HDUList['SCI'].data
+            HDUList.close()
+        
+        primHDU = fits.PrimaryHDU(header=self.firstHeader)
+        primHDU.name = 'PRIMARY'
+        
+        cubeHDU = fits.ImageHDU(data=allCube,header=self.firstSciHeader)
+        HDUList = fits.HDUList([primHDU,cubeHDU])
+        HDUList.writeto(self.outImg,overwrite=True)
+        
+def test():
+    """ example run of comb_img """
+    c = combInts('sim_img/grism/NRCALONG_F444W*.fits','sim_img/grism_comb/NRCALONG_F444W.fits')
+    c.comb()
+
+def comb_all():
+    """ Combines integrations of all small simulations """
+    schNames = ['NRCA1_F322W2',
+                'NRCALONG_F322W2',
+                'NRCA1_F444W',
+                'NRCALONG_F444W',
+                'NRCB1','NRCBLONG']
+    dirNames = ['grism','grism','grism','grism',
+               'direct_imaging','direct_imaging']
+    for oneSch,oneDir in zip(schNames,dirNames):
+        searchPath = 'sim_img/'+oneDir+'/'+oneSch+'*.fits'
+        savePath = 'sim_img/'+oneDir+'_comb/'+oneSch+'.fits'
+        c = combInts(searchPath,savePath)
+        c.comb()
+    
