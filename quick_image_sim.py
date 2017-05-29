01e4c2a9f6fc4300f20704e0c0f3e8ea3d4e4a95
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2017-05-29 10:13:36 -0700

    changed from an absolute to relative path.

--- quick_image_sim.py
+++ quick_image_sim.py
@@ -10,7 +10,7 @@ for oneFilt in ['F210M','F300M']:
     bpJ = S.ObsBandpass('johnson,j')
     sp = pynrc.stellar_spectrum('F7V', 9.3, 'vegamag', bpJ)
     thisDetector = nrc.Detectors[0]
-    file_out = '/Users/everettschlawin/outside_progs/pynrc_notebooks/sim_img/direct_imaging/'+thisDetector.detname
+    file_out = 'sim_img/direct_imaging/'+thisDetector.detname
     # Specify name of output file.
     # Time stamps will be automatically inserted for unique file names.
     res_sp = nrc.gen_exposures(sp, file_out,targ_name='WASP-62')
