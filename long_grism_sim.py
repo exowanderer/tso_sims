27b7fe8547e0715734b983741857b22dea21b776
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2017-05-31 14:11:49 -0700

    added a script to make a longer grism simulation.

---
+++ long_grism_sim.py
@@ -0,0 +1,21 @@
+import pynrc
+pynrc.setup_logging('WARN', verbose=False)
+from pynrc.nrc_utils import S
+
+oneFilt = 'F444W'
+nGROUP = 12
+nINT = 30
+# Initialize a NIRCam Grism observation in Stripe mode 
+nrc = pynrc.NIRCam(oneFilt, pupil='GRISM0', module='A', nint=nINT,ngroup=nGROUP,
+                   wind_mode='STRIPE', xpix=2048, ypix=256,read_mode='BRIGHT1')
+
+nrcWL = pynrc.NIRCam('F210M', pupil='Weak Lens +8', module='A', nint=nINT,ngroup=nGROUP,
+                     wind_mode='STRIPE', xpix=2048, ypix=256)
+# Specify name of output file.
+# Time stamps will be automatically inserted for unique file names.
+bpJ = S.ObsBandpass('johnson,j')
+sp = pynrc.stellar_spectrum('K7V', 9.2, 'vegamag', bpJ)
+file_out = 'sim_img/grism/NRCALONG_'+oneFilt+'_'
+file_outWL = 'sim_img/grism/NRCA1_'+oneFilt+'_'
+res_sp = nrc.gen_exposures(sp, file_out,targ_name='WASP-80')
+res_wl = nrcWL.gen_exposures(sp, file_outWL,targ_name='WASP-80')
