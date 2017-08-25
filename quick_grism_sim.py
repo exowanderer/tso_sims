1c2dd3ee131bc61944d4819e12a7c9deda946ece
Jonathan Fraine <exowanderer@users.noreply.github.com>
2017-08-25 16:16:21 -0400

    Removed commented out extra bits

--- quick_grism_sim.py
+++ quick_grism_sim.py
@@ -3,29 +3,6 @@ import pynrc
 pynrc.setup_logging('WARN', verbose=False)
 from pynrc.nrc_utils import S
 
-# # Initialize a NIRCam Grism observation in Stripe mode
-# for oneFilt,nGROUP in zip(['F444W','F322W2'],[12,6]):
-#     print('Starting Loop Iteration with filter {} & ngroups {}'.format(oneFilt,nGROUP))
-#
-#     nrcWL = pynrc.NIRCam('F210M', pupil='Weak Lens +8', module='A', nint=2,ngroup=nGROUP,
-#                          wind_mode='STRIPE', xpix=2048, ypix=256)
-#
-#     print('Allocated SW NRC instance for {} & {}'.format(oneFilt,nGROUP))
-#
-#     nrc = pynrc.NIRCam(oneFilt, pupil='GRISM0', module='A', nint=2,ngroup=nGROUP,
-#                        wind_mode='STRIPE', xpix=2048, ypix=256,read_mode='BRIGHT1')
-#
-#     print('Allocated LW NRC instance for {} & {}'.format(oneFilt,nGROUP))
-#
-#     # Specify name of output file.
-#     # Time stamps will be automatically inserted for unique file names.
-#     bpJ = S.ObsBandpass('johnson,j')
-#     sp = pynrc.stellar_spectrum('K7V', 9.2, 'vegamag', bpJ)
-#     file_out = 'sim_img/grism/NRCALONG_'+oneFilt+'_'
-#     file_outWL = 'sim_img/grism/NRCA1_'+oneFilt+'_'
-#     res_sp = nrc.gen_exposures(  sp, file_out)
-#     res_wl = nrcWL.gen_exposures(sp, file_outWL)
-
 # Initialize a NIRCam Grism observation in Stripe mode 
 for oneFilt,nGROUP in zip(['F444W','F322W2'],[12,6]):
     print()
